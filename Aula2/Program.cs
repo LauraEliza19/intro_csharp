5b569f41ba74eb5fa258851f7cf16a0b555c629e
Laura Palagano <69463821+LauraEliza19@users.noreply.github.com>
2025-05-06 10:51:44 -0300

    aula 2

--- Aula2/Program.cs
+++ Aula2/Program.cs
@@ -1,5 +1,5 @@
 ﻿//----------------------------------------------------//--------------------------------------------------
-//ex 1
+//ex 2
 using System;
 
 class Program
@@ -8,44 +8,53 @@ class Program
     {
         Random rand = new Random();
 
-        Console.WriteLine("Escolha entre 'par' ou 'impar': ");
-        string escolha = Console.ReadLine().ToLower();
-
-        if (escolha != "par" && escolha != "impar")
+        Console.Write("Quantas vezes deseja jogar o dado? ");
+        if (!int.TryParse(Console.ReadLine(), out int jogadas))
         {
-            Console.WriteLine("Escolha inválida. Tente novamente.");
+            Console.WriteLine("Entrada inválida.");
             return;
         }
 
-        Console.WriteLine("Informe um número inteiro: ");
-        if (!int.TryParse(Console.ReadLine(), out int numero))
+        int[] faces = new int[7];
+
+        for (int i = 1; i <= jogadas; i++)
         {
-            Console.WriteLine("Número inválido. Tente novamente.");
-            return;
+            int resultado = rand.Next(1, 7);
+            faces[resultado]++;
+            Console.WriteLine($"Jogada {i}: {resultado}");
         }
 
-        int outroNumero = rand.Next(1, 11); 
-
-        int soma = numero + outroNumero;
-
-        Console.WriteLine($"A soma de {numero} + {outroNumero} é: {soma}");
-
-        if (soma % 2 == 0)
+        Console.WriteLine("\nContagem de cada face:");
+        for (int i = 1; i <= 6; i++)
         {
-            Console.WriteLine("O resultado da soma é PAR.");
-        }
-        else
-        {
-            Console.WriteLine("O resultado da soma é ÍMPAR.");
-        }
-
-        if ((escolha == "par" && soma % 2 == 0) || (escolha == "impar" && soma % 2 != 0))
-        {
-            Console.WriteLine("Você acertou!");
-        }
-        else
-        {
-            Console.WriteLine("Você errou.");
+            Console.WriteLine($"Face {i}: {faces[i]}");
         }
     }
 }
+
+
+        // int outroNumero = rand.Next(1, 11); 
+
+        // int soma = jogadas + outroNumero;
+
+        // Console.WriteLine($"A soma de {jogadas} + {outroNumero} é: {soma}");
+
+        // if (soma % 2 == 0)
+        // {
+        //     Console.WriteLine("O resultado da soma é PAR.");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("O resultado da soma é ÍMPAR.");
+        // }
+
+        // if ((escolha == "par" && soma % 2 == 0) || (escolha == "impar" && soma % 2 != 0))
+        // {
+        //     Console.WriteLine("Você acertou!");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Você errou.");
+        // }
+//     }
+// }
